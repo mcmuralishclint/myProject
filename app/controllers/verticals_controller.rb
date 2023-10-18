cc7a16d61d683e333ae0dd3df8cbc9cb463962b1
muralish.clinton <muralish.clinton@circles.asia>
2023-10-18 23:05:30 +0530

    verticals controller init

---
+++ app/controllers/verticals_controller.rb
@@ -0,0 +1,44 @@
+class VerticalsController < ApplicationController
+  before_action :set_vertical, only: [:show, :update, :destroy]
+
+  def index
+    @verticals = Vertical.all
+    render json: @verticals
+  end
+
+  def show
+    render json: @vertical
+  end
+
+  def create
+    @vertical = Vertical.new(vertical_params)
+
+    if @vertical.save
+      render json: @vertical, status: :created
+    else
+      render json: @vertical.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @vertical.update(vertical_params)
+      render json: @vertical
+    else
+      render json: @vertical.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @vertical.destroy
+  end
+
+  private
+
+  def set_vertical
+    @vertical = Vertical.find(params[:id])
+  end
+
+  def vertical_params
+    params.require(:vertical).permit(:name)
+  end
+end
