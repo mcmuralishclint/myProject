a88add2e8861b75375f9803bdf70ccc09b697187
muralish.clinton <muralish.clinton@circles.asia>
2023-10-19 15:28:23 +0530

    init course on vertical model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,23 @@
 class Course < ApplicationRecord
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
   belongs_to :category
   enum state: { active: 1, inactive: 0 }
 
   validates :name, uniqueness_across_models: true
+
+  after_commit :index_document, on: [:create, :update]
+
+  settings index: { number_of_shards: 1 } do
+    mappings dynamic: 'false' do
+      indexes :name, analyzer: 'english'
+    end
+  end
+
+  private
+
+  def index_document
+    self.import rescue nil
+  end
 end
