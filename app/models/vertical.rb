5f6f7bbb17fc03a40696b6abe05b210ee824ada4
muralish.clinton <muralish.clinton@circles.asia>
2023-10-19 10:38:55 +0530

    add presence check for name - vertical model

--- app/models/vertical.rb
+++ app/models/vertical.rb
@@ -2,4 +2,5 @@ class Vertical < ApplicationRecord
   has_one :category, dependent: :destroy
   validates :name, uniqueness_across_models: true
   validates_uniqueness_of :name
+  validates_presence_of :name
 end
