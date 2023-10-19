7122301a3a3cce67b03eaacda0b39079dc389828
muralish.clinton <muralish.clinton@circles.asia>
2023-10-19 15:28:12 +0530

    init elasticsearch on vertical model

--- app/models/vertical.rb
+++ app/models/vertical.rb
@@ -1,6 +1,23 @@
 class Vertical < ApplicationRecord
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
   has_one :category, dependent: :destroy
   validates :name, uniqueness_across_models: true
   validates_uniqueness_of :name
   validates_presence_of :name
+
+  after_commit :index_document, on: [:create, :update]
+
+  settings index: { number_of_shards: 1 } do
+    mappings dynamic: 'false' do
+      indexes :name, analyzer: 'english'
+    end
+  end
+
+  private
+
+  def index_document
+    self.import rescue nil
+  end
 end
