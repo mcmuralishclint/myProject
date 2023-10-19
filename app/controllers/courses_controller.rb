36431dc88267b6c5a9d832af3c995c1bdf44dabb
muralish.clinton <muralish.clinton@circles.asia>
2023-10-19 12:17:47 +0530

    remove unwanted actions from courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,5 @@
 class CoursesController < ApplicationController
-  before_action :set_course, only: [:show, :update, :destroy]
+  before_action :set_course, only: [:show]
 
   def index
     @courses = Course.all
@@ -10,35 +10,9 @@ class CoursesController < ApplicationController
     render json: @course
   end
 
-  def create
-    @course = Course.new(course_params)
-
-    if @course.save
-      render json: @course, status: :created
-    else
-      render json: @course.errors, status: :unprocessable_entity
-    end
-  end
-
-  def update
-    if @course.update(course_params)
-      render json: @course
-    else
-      render json: @course.errors, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @course.destroy
-  end
-
   private
 
   def set_course
     @course = Course.find(params[:id])
-  end
-
-  def course_params
-    params.require(:course).permit(:name, :author, :category_id, :state)
   end
 end
