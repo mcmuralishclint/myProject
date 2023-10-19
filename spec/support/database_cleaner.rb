224b552bd4dbba0e8ffceb65b841282d66fd946f
Muralish <mcmuralishclint@outlook.com>
2023-10-19 23:50:33 +0530

    remove  db record deletion from database cleaner functions

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,7 +1,6 @@
 RSpec.configure do |config|  
     config.before(:suite) do
         DatabaseCleaner.allow_remote_database_url = true
-        DatabaseCleaner.clean_with(:deletion)
         DatabaseCleaner.clean_with :truncation, except: %w(ar_internal_metadata)
     end
     
