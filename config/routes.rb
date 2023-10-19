ccafc5765e60721d51826668e26582230632523f
muralish.clinton <muralish.clinton@circles.asia>
2023-10-19 15:27:40 +0530

    route for search functionality

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     scope :v1 do
       post '/create_resources', to: 'resources#create'
       patch '/update_resources/:id', to: 'resources#update'
+      get '/search', to: 'search#index'
     end
   end
 end
