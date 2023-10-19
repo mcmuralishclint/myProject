8723a9cb51fe9f6b16de4dc8fc67702b2dff28b6
muralish.clinton <muralish.clinton@circles.asia>
2023-10-19 15:28:53 +0530

    init search controller

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,21 @@
+class SearchController < ApplicationController
+  def index
+    query = params[:q]
+    results = search(query)
+
+    render json: { results: results }
+  end
+
+  private
+
+  def search(query)
+    search_service = Search::SearchServiceFactory.build(query, engine: DEFAULT_SEARCH_DATABASE)
+    results = { verticals: [], categories: [], courses: [] }
+
+    results[:verticals] = search_service.search_in_model(Vertical)
+    results[:categories] = search_service.search_in_model(Category)
+    results[:courses] = search_service.search_in_model(Course)
+
+    results
+  end
+end
