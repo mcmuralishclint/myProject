cdb8474440868f91141edc1c700d5f96ade4682e
muralish.clinton <muralish.clinton@circles.asia>
2023-10-20 18:44:17 +0530

    move all routes within a scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  resources :verticals do
-    resources :categories, only: [:index, :show] do
-      resources :courses, only: [:index, :show]
-    end
-  end
-
   scope :api do
     scope :v1 do
       get '/search', to: 'search#index'
       get '/login', to: 'auth#login'
+
+      resources :verticals do
+        resources :categories, only: [:index, :show] do
+          resources :courses, only: [:index, :show]
+        end
+      end
     end
   end
 end
